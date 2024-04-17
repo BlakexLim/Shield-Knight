7a959c9268ad6d9ae0739a79a720baafc2287d88
Blake Lim <blakelim007@gmail.com>
2024-04-17 23:11:14 +0000

    Rough CSS layout for newgame page

--- client/src/Pages/NewGame.tsx
+++ client/src/Pages/NewGame.tsx
@@ -2,17 +2,21 @@ import { Link } from 'react-router-dom';
 
 export function NewGame() {
   return (
-    <>
-      <div>
+    <div className="container flex flex-col items-center m-7">
+      <div className="m-7">
         <Link to="/intofire">
-          <button>Continue</button>
+          <button className="text-2xl bg-yellow-300 rounded-3xl h-10 w-64">
+            Continue
+          </button>
         </Link>
       </div>
-      <div>
+      <div className="m-4">
         <Link to="/intofire">
-          <button>New Game</button>
+          <button className="text-2xl bg-yellow-300 rounded-3xl h-10 w-64">
+            New Game
+          </button>
         </Link>
       </div>
-    </>
+    </div>
   );
 }
