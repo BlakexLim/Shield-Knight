658b1eea0ba5b49c68e007d7bd1a696f4a9b0d08
Blake Lim <blakelim007@gmail.com>
2024-04-17 23:08:07 +0000

    Rough CSS layout for login page

--- client/src/Pages/Login.tsx
+++ client/src/Pages/Login.tsx
@@ -3,25 +3,31 @@ import { Link } from 'react-router-dom';
 export function Login() {
   return (
     <>
-      <form action="submit">
-        <div>
+      <form className="container flex flex-col items-center bg-gray-400 h-72 w-64 m-7 rounded-2xl">
+        <div className="m-2 text-center">
           <label>
             Username
             <input type="text" />
           </label>
         </div>
-        <div>
+        <div className="m-2 text-center">
           <label>
             Password
             <input type="text" />
           </label>
         </div>
-        <div>
-          <button>Login</button>
+        <div className="m-3">
+          <Link to="/newgame">
+            <button className="bg-yellow-300 rounded-2xl p-2 w-28">
+              Login
+            </button>
+          </Link>
         </div>
-        <div>
+        <div className="m-7">
           <Link to="/intofire">
-            <button>Continue as guest</button>
+            <button className="bg-yellow-300 rounded-3xl p-2">
+              Continue as guest
+            </button>
           </Link>
         </div>
       </form>
--- client/src/Pages/Title.tsx
+++ client/src/Pages/Title.tsx
@@ -4,25 +4,25 @@ export function Title() {
   return (
     <div className="container flex flex-col items-center m-12 font-serif">
       <div>
-        <h1 className="text-6xl/snug bg-red-900 rounded-3xl p-1">
+        <h1 className="text-6xl/snug bg-red-900 rounded-3xl p-1 text-yellow-400">
           Shield Knight
         </h1>
       </div>
-      <div className="m-6">
+      <div className="hidden m-6">
         <Link to="/login">
           <button className="text-2xl bg-yellow-300 rounded-3xl h-10 w-64">
             Login
           </button>
         </Link>
       </div>
-      <div className="m-6">
+      <div className="hidden m-6">
         <Link to="/sign-up">
           <button className="text-2xl bg-yellow-300 rounded-3xl h-10 w-64">
             New account
           </button>
         </Link>
       </div>
-      <div className="m-6">
+      <div className="hidden m-6">
         <Link to="/controls">
           <button className="text-2xl bg-yellow-300 rounded-3xl h-10 w-64">
             Guest
