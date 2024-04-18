cb5f103d5ab766046a91f0caedb141be03ba9a0f
Blake Lim <blakelim007@gmail.com>
2024-04-18 18:08:15 +0000

    Basic layout fixed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,14 +12,13 @@ import { Title } from './Pages/Title';
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Title />}>
-        <Route path="newgame" element={<NewGame />} />
-        <Route path="controls" element={<Controls />} />
-        <Route path="login" element={<Login />} />
-        <Route path="sign-up" element={<SignUp />} />
-        <Route path="intofire" element={<Game />} />
-        <Route path="*" element={<NotFound />} />
-      </Route>
+      <Route path="/" element={<Title />} />
+      <Route path="newgame" element={<NewGame />} />
+      <Route path="controls" element={<Controls />} />
+      <Route path="login" element={<Login />} />
+      <Route path="sign-up" element={<SignUp />} />
+      <Route path="intofire" element={<Game />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
--- client/src/Pages/Title.tsx
+++ client/src/Pages/Title.tsx
@@ -1,36 +1,34 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export function Title() {
   return (
-    // className="container flex flex-col items-center m-12 font-serif"
-    <div>
+    <div className="container flex flex-col items-center mt-10 font-serif">
       <div>
-        <h1 className="hidden text-6xl/snug bg-red-900 rounded-3xl p-1 text-yellow-400">
+        <h1 className="text-6xl/snug bg-red-900 rounded-3xl p-1 text-yellow-400">
           Shield Knight
         </h1>
       </div>
-      <div className="hidden m-6">
+      <div className="m-6">
         <Link to="/login">
           <button className="text-2xl bg-yellow-300 rounded-3xl h-10 w-64">
             Login
           </button>
         </Link>
       </div>
-      <div className="hidden m-6">
+      <div className="m-6">
         <Link to="/sign-up">
           <button className="text-2xl bg-yellow-300 rounded-3xl h-10 w-64">
             New account
           </button>
         </Link>
       </div>
-      <div className="hidden m-6">
+      <div className="m-6">
         <Link to="/controls">
           <button className="text-2xl bg-yellow-300 rounded-3xl h-10 w-64">
             Guest
           </button>
         </Link>
       </div>
-      <Outlet />
     </div>
   );
 }
