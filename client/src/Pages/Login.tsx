d4fbd405f17e3ddee8ed9492278311992fa7169f
Blake Lim <blakelim007@gmail.com>
2024-04-19 02:06:51 +0000

    Completed CSS for SignUp page

--- client/src/Pages/Login.tsx
+++ client/src/Pages/Login.tsx
@@ -2,30 +2,35 @@ import { Link } from 'react-router-dom';
 
 export function Login() {
   return (
-    <div className="flex justify-center">
-      <form className="flex flex-col justify-center items-center bg-gray-400 h-1/2 w-1/2 m-7 rounded-2xl">
-        <div className="m-2 text-center drop-shadow-xl">
+    <div className="flex justify-center items-center h-screen">
+      <form className="flex flex-col items-center bg-gray-400 h-96 w-80 rounded-2xl">
+        <div>
+          <h1 className="text-3xl tracking-widest animate-pulse pt-10">
+            Login
+          </h1>
+        </div>
+        <div className="p-4 text-center drop-shadow-xl w-4/5">
           <h2>Username</h2>
           <label>
-            <input type="text" className="cursor-pointer" />
+            <input type="text" className="cursor-pointer w-56" />
           </label>
         </div>
-        <div className="m-2 text-center drop-shadow-xl">
+        <div className="p-4 text-center drop-shadow-xl w-4/5">
           <h2>Password</h2>
           <label>
-            <input type="text" className="cursor-pointer" />
+            <input type="text" className="cursor-pointer w-56" />
           </label>
         </div>
-        <div className="m-2">
+        <div className="p-2">
           <Link to="/newgame">
             <button className="bg-yellow-300 hover:bg-yellow-500 rounded-2xl w-28 drop-shadow-2xl">
               Login
             </button>
           </Link>
         </div>
-        <div className="m-2">
+        <div className="pt-11">
           <Link to="/controls">
-            <button className="bg-yellow-300 hover:bg-yellow-500 rounded-3xl w-28 drop-shadow-2xl">
+            <button className="bg-yellow-300 hover:bg-yellow-500 rounded-3xl w-36 drop-shadow-2xl">
               Continue as guest
             </button>
           </Link>
--- client/src/Pages/SignUp.tsx
+++ client/src/Pages/SignUp.tsx
@@ -2,24 +2,31 @@ import { Link } from 'react-router-dom';
 
 export function SignUp() {
   return (
-    <div className="flex justify-center">
-      <form action="submit">
+    <div className="flex justify-center items-center h-screen">
+      <form className="flex flex-col items-center bg-gray-400 h-96 w-80 rounded-2xl">
         <div>
+          <h1 className="text-3xl tracking-widest animate-pulse pt-10">
+            Create account
+          </h1>
+        </div>
+        <div className="p-4 text-center drop-shadow-xl w-4/5">
+          <h2>Username</h2>
           <label>
-            username
-            <input required type="text" />
+            <input type="text" className="cursor-pointer w-56" />
           </label>
         </div>
-        <div>
+        <div className="p-4 text-center drop-shadow-xl w-4/5">
+          <h2>Password</h2>
           <label>
-            Password
-            <input required type="text" />
+            <input type="text" className="cursor-pointer w-56" />
           </label>
         </div>
-        <div>
+        <div className="p-2">
           <label>
             <Link to="/login">
-              <button>Create account</button>
+              <button className="bg-yellow-300 hover:bg-yellow-500 rounded-3xl w-28 drop-shadow-2xl">
+                Create account
+              </button>
             </Link>
           </label>
         </div>
